e91327cfa1652d637fe99f6e8ecaa0df46b45f63
Jason Carini <carinij@gmail.com>
2021-01-16 11:55:42 -0800

    Complete pseudoclassical refactor

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,11 +2,12 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  console.log("the button works");
+  console.log(this);
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-
 makeBlinkyDancer.prototype.oldStep = makeBlinkyDancer.prototype.step;
 
 makeBlinkyDancer.prototype.step = function() {
--- src/dancer.js
+++ src/dancer.js
@@ -1,34 +1,28 @@
-
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
+
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
 
+  this.step();
+  this.setPosition();
 };
 
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(function() {
-    makeDancer.prototype.step.call();
-  }
-  , this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
-
-// Needed to call step()
-makeDancer.prototype.step.call(null);
 
 makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   var styleSettings = {
-    top: top,
-    left: left
+    top: this.top,
+    left: this.left
   };
   this.$node.css(styleSettings);
 };
-
-makeDancer.prototype.setPosition.call(null, this.top, this.left);
-
-// now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-// this one sets the position to some random default point within the body
